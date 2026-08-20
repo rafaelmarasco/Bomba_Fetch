1aa120303b372205491ece9a918f27d02bc5e0b2
Rafael Marasco <55808458+rafaelmarasco@users.noreply.github.com>
2026-08-20 10:52:42 -0400

    bug fix...

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,22 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private PlayerInputs playerInputs;
+    [SerializeField] private InputSystem_Actions playerInputs;
     private Rigidbody rb;
+    private Vector2 movementInputs;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        playerInputs.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized()
+    {
+      return movementInputs = playerInputs.Player.Move.ReadValue<Vector2>();
     }
 
 
