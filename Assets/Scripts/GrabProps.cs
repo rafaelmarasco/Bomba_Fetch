5bdd19f96ad98e94d4f649212f062dad590ea865
Rafael Marasco <55808458+rafaelmarasco@users.noreply.github.com>
2026-08-26 13:43:12 -0400

    code fix

--- Assets/Scripts/GrabProps.cs
+++ Assets/Scripts/GrabProps.cs
@@ -11,54 +11,51 @@ public class GrabProps : MonoBehaviour
     private void Update()
     {
         lastMoveDir = player.moveDir != Vector3.zero ? player.moveDir : lastMoveDir;
+        // If they dont have any item they pick up an item
+        if (!hasItem && CheckForProps(out GameObject obj) && obj.CompareTag("Prop"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+                PickUpProp(obj);
+        }
 
         // Drop the item if they have any item in hand
-
-        // Else if they dont have pick up an item
-        if (!hasItem && CheckForProps(out RaycastHit hit) && hit.collider.gameObject.CompareTag("Prop"))
-            PickUpProp(hit);
-
         else if (hasItem)
-            DropProp();
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+                DropProp();
+        }
     }
 
-    private bool CheckForProps(out RaycastHit propHit) // Check if theres an object in front of the player
+    private bool CheckForProps(out GameObject obj) // Check if theres an object in front of the player
     {
         float maxDistance = 1f;
-        bool canGrab = Physics.Raycast(transform.position, lastMoveDir, out propHit, maxDistance);
+        bool canGrab = Physics.Raycast(transform.position, lastMoveDir, out RaycastHit hit, maxDistance);
+        obj = hit.collider.gameObject;
         return canGrab;
     }
 
     private void DropProp()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (heldItem.TryGetComponent<Rigidbody>(out Rigidbody propRb))
-                propRb.isKinematic = false;
+        if (heldItem.TryGetComponent<Rigidbody>(out Rigidbody propRb))
+            propRb.isKinematic = false;
 
-            heldItem.transform.SetParent(null);
-            hasItem = false;
-            heldItem = null;
-        }
+        heldItem.transform.SetParent(null);
+        hasItem = false;
+        heldItem = null;
     }
 
-    private void PickUpProp(RaycastHit hit)
+    private void PickUpProp(GameObject prop)
     {
-        GameObject prop = hit.collider.gameObject;
+        float offSet = .5f;
 
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            float offSet = .5f;
+        if (prop.TryGetComponent<Rigidbody>(out Rigidbody propRb))
+            propRb.isKinematic = true;
 
-            if (prop.TryGetComponent<Rigidbody>(out Rigidbody propRb))
-                propRb.isKinematic = true;
+        prop.transform.SetParent(propTestPos);
+        prop.transform.localPosition = new Vector3(0f, 0f, 0f + offSet);
 
-            prop.transform.SetParent(propTestPos);
-            prop.transform.localPosition = new Vector3(0f, 0f, 0f + offSet);
-
-            heldItem = prop;
-            hasItem = true;
-        }
+        heldItem = prop;
+        hasItem = true;
     }
 
     private void OnDrawGizmosSelected()
