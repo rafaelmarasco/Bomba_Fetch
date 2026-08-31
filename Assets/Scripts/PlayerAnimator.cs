0176a8f495d4eda59dc0dbbd8e1cf0ae90605894
Rafael Marasco <55808458+rafaelmarasco@users.noreply.github.com>
2026-08-30 22:07:37 -0400

    Ajustando a animaçao com animation rigging

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -4,14 +4,10 @@ public class PlayerAnimator : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private Player player;
-    [SerializeField] private PropInteract propInteract;
     private const string IS_WALKING = "isWalking";
-    //private const string IS_CARRYING = "isCarrying";
 
     private void Update()
     {
         animator.SetBool(IS_WALKING, player.GetIsWalking());
-
-      //animator.SetBool(IS_CARRYING, propInteract.hasItem);
     }
 }
