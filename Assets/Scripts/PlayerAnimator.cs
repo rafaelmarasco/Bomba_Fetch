e015d903c6e90169c52e971b392df4a3f1a43ada
Rafael Marasco <55808458+rafaelmarasco@users.noreply.github.com>
2026-08-30 22:17:14 -0400

    animaçao funcionando ao pegar e soltar objetos

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class PlayerAnimator : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private Player player;
+    [SerializeField] private PropInteract propInteract;
+    [SerializeField] private Rig grabRig;
     private const string IS_WALKING = "isWalking";
 
     private void Update()
     {
         animator.SetBool(IS_WALKING, player.GetIsWalking());
+
+        if (propInteract.hasItem)
+            grabRig.weight = 1f;
+        else 
+            grabRig.weight = 0f;
     }
 }
