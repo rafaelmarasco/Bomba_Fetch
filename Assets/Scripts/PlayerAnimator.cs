2950913388aacfaf6726b463e24822c897dfce94
Rafael Marasco <55808458+rafaelmarasco@users.noreply.github.com>
2026-09-02 01:53:28 -0400

    Animaçao de empurrar

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 using UnityEngine.InputSystem;
@@ -8,8 +10,13 @@ public class PlayerAnimator : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private Player player;
     [SerializeField] private PropInteract propInteract;
+
+    [Header("Rig Field")]
     [SerializeField] private Rig grabRig;
+    [SerializeField] private Rig pushRig;
+
     private InputSystem_Actions inputActions;
+
     private const string IS_WALKING = "isWalking";
 
     private void Awake()
@@ -22,6 +29,7 @@ public class PlayerAnimator : MonoBehaviour
 
     private void Push_performed(InputAction.CallbackContext context)
     {
+        StartCoroutine(AnimatePush());
         UpdateHands();
     }
 
@@ -40,5 +48,27 @@ public class PlayerAnimator : MonoBehaviour
             grabRig.weight = 1f;
         else
             grabRig.weight = 0f;
+    }
+
+    private IEnumerator AnimatePush()
+    {
+        float duration = 0.15f;
+        float timePassed = 0;
+
+        while(timePassed <= duration)
+        {
+            pushRig.weight = Mathf.Lerp(0f, 1f, timePassed / duration);
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+
+        timePassed = 0;
+
+        while(timePassed <= duration)
+        {
+            pushRig.weight = Mathf.Lerp(1f, 0f, timePassed / duration);
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
     }
 }
