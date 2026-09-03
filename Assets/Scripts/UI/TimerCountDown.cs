0ed53c50104463b3e983d6a69aed95f7abcc3366
Bruno Kakazu <43326668+BrunoKakazu@users.noreply.github.com>
2026-09-03 06:00:42 -0400

    Timer da Bomba feito

--- Assets/Scripts/UI/TimerCountDown.cs
+++ Assets/Scripts/UI/TimerCountDown.cs
@@ -4,24 +4,29 @@ using TMPro;
 
 public class TimerCountDown : MonoBehaviour
 {
-    [Header("Timer Count Down")]
-    [SerializeField] private GameObject timerScreen;
-    [SerializeField] private TextMeshProUGUI timerText;
+    [Header("Timer Count Down Start")]
+    [SerializeField] private GameObject timerScreenStarter;
+    [SerializeField] private TextMeshProUGUI timerTextStart;
+
+    [Header("Timer Count Down Final")]
+    [SerializeField] private GameObject timerScreenFinal;
+    [SerializeField] private TextMeshProUGUI timerTextFinal;
 
 
     [Header("Game Over UI")]
     [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private Button restartButton;
+
+
+    [Header("Controllers")]
+    [SerializeField] private SceneController sceneController = new();
 
 
     private float timer;
+    [SerializeField] private float setTimer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        StartCountDown();
-    }
-
-    private void OnEnable()
     {
         StartCountDown();
     }
@@ -34,21 +39,39 @@ public class TimerCountDown : MonoBehaviour
 
     private void StartCountDown()
     {
-        timer = 60f; // Reset the timer to 60 seconds
+        timer = setTimer; // Reset the timer to 60 seconds
         gameOverScreen.SetActive(false);
-        timerScreen.SetActive(true);
+        timerScreenFinal.SetActive(false);
+        timerScreenStarter.SetActive(true);
     }
 
     private void CountDown()
     {
-        if (timer > 0)
+        if (timer > 6)
         {
             timer -= Time.deltaTime;
-            timerText.text = timer.ToString("0.00");
+            timerTextStart.text = timer.ToString("0.00");
+        }
+        else if (timer < 6 && timer > 0)
+        {
+            if (timerScreenFinal.activeInHierarchy == false)
+            {
+                timerScreenFinal.SetActive(true);
+                timerScreenStarter.SetActive(false);
+            }
+
+            timer -= Time.deltaTime;
+            timerTextFinal.text = timer.ToString("0");
         }
         else
         {
+            timerScreenFinal.SetActive(false);
             gameOverScreen.SetActive(true);
+            restartButton.onClick.RemoveAllListeners();
+            restartButton.onClick.AddListener(() => sceneController.LoadScene(0));
         }
     }
 }
+
+
+
