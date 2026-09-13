42feaf4caf903f3a000bbd6f6c909b1836c81a58
Bruno Kakazu <43326668+BrunoKakazu@users.noreply.github.com>
2026-09-13 16:16:11 -0400

    Mudei o canvas para a bomba e não mais para o player

--- Assets/Scripts/BombaScriptMath.cs
+++ Assets/Scripts/BombaScriptMath.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class BombaScriptMath : MonoBehaviour
@@ -7,14 +8,34 @@ public class BombaScriptMath : MonoBehaviour
     [SerializeField] private TextMeshProUGUI disarmText;
     [SerializeField] private Button disarmBtn;
 
+
+    //[SerializeField] private Button[] numbersButton;
+
     [Header("Mudar no futuro para um UIManager")]
     [SerializeField] private PropInteract propInteract;
     [SerializeField] private Canvas bombInterface;
+    [SerializeField] private PlayerInput uiInput;
 
     private void Update()
     {
-        bombInterface.gameObject.SetActive(propInteract.isBombInteracting);
+        CheckBomb();
     }
+
+    private void CheckBomb()
+    {
+        bombInterface.gameObject.SetActive(propInteract.isBombInteracting);
+        
+        if (propInteract.isBombInteracting)
+        {
+            BombUIConfig();    
+        }
+    }
+
+    private void BombUIConfig()
+    {
+        uiInput = GetComponent<PlayerInput>();
+    }
+
     public void DisarmBomb()
     {
         disarmText.gameObject.SetActive(true);
