fecf79190785a697cabfa0805d6b2ba4caebbd8f
Rafael Marasco <55808458+rafaelmarasco@users.noreply.github.com>
2026-09-17 21:56:07 -0400

    Power Box concluida
    Leva em cosideraçao mais de um jogador.
    Foi separada em uma area de interaçao e o proprio power box que faz a interaçao

--- Assets/Scripts/Hazzard.cs
+++ Assets/Scripts/Hazzard.cs
@@ -5,9 +5,10 @@ public class Hazzard : MonoBehaviour
     [SerializeField] private Vector3 flyDirection;
     [SerializeField] private float flyForce;
     [SerializeField] private float stunTime;
+    public bool isOn;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "GFX")
+        if (isOn && other.gameObject.name == "GFX")
         {
             PlayerEventManager playerEventManager = other.GetComponentInParent<PlayerEventManager>();
             playerEventManager.Eletrocute(flyDirection, flyForce , stunTime);
